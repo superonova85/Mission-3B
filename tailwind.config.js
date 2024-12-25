11fbfc7c6311f3a6d22ba6282adbcadc4ec12cff
superonova85 <nel.comp85@gmail.com>
2024-12-26 00:10:42 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+
+  theme: {
+    extend: {
+      colors: {
+        text: "#8E9199",
+        input: "#FFFFFF",
+        star: "red",
+        main: "#fef9c3",
+      },     
+    },
+
+    plugins: [],
+  },
+};
